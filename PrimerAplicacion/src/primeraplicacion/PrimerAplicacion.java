6b77d3d23cd0c1a9793666956286d1fe33ce78e6
Manuel Rivallo <mrivallo@hotmail.com>
2022-04-10 19:42:52 +0200

    Hola mundo

--- PrimerAplicacion/src/primeraplicacion/PrimerAplicacion.java
+++ PrimerAplicacion/src/primeraplicacion/PrimerAplicacion.java
@@ -15,7 +15,8 @@ public class PrimerAplicacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("");
+        System.out.println("Un cambio");
     }
     
 }
